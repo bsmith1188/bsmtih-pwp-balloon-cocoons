1d9ad2d862c0dc496c81877d13b3ed0e66fce6c6
Ben Smith <rios7467@gmail.com>
2017-02-05 23:06:13 -0700

    Fixed the directory source for the image tags.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,14 +6,14 @@
 		<title>Bsmtih PWP Balloon Cocoons Milestone 2</title>
 	</head>
 	<body>
-		<h3>Content Documentation</h3>
-		<h5>Images of the stock balloons.</h5>
+		<h1>Content Documentation</h1>
+		<h3>Images of the stock balloons.</h3>
 		<p>My mother already has a fair amount of documentation of her baloons especailly the popular ones so it would be ideal to have the "premade" balloons listed down the page with a price. The image will be accompanied by a brief description of the content.</p>
-		<h5>"Contact Me" section</h5>
+		<h3>"Contact Me" section</h3>
 		<p>A small footer that "if possible" will always be visible with her contact information so that prospective customers can get her info easily. In the same footer in a different section is just a small text snippet that says custom balloons are available to let people know that their options are not limited at all.</p>
-		<h6>Wireframe for mobile</h6>
+		<h3>Wireframe for mobile</h3>
 		<img src="../images/pwp-wf-a.png">
-		<h6>Wireframe for desktop</h6>
+		<h3>Wireframe for desktop</h3>
 		<img src="../images/pwp-wf-b.png">
 	</body>
 </html>
