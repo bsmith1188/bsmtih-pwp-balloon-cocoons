d4ab2d1d6adf3f11e2c75d6d7526afa9b5499f24
Ben Smith <rios7467@gmail.com>
2017-02-05 22:56:32 -0700

    Added milestone 2a documentation and wireframe images.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta charset=UTF-8"
+		<title>Bsmtih PWP Balloon Cocoons Milestone 2</title>
+	</head>
+	<body>
+		<h3>Content Documentation</h3>
+		<h5>Images of the stock balloons.</h5>
+		<p>My mother already has a fair amount of documentation of her baloons especailly the popular ones so it would be ideal to have the "premade" balloons listed down the page with a price. The image will be accompanied by a brief description of the content.</p>
+		<h5>"Contact Me" section</h5>
+		<p>A small footer that "if possible" will always be visible with her contact information so that prospective customers can get her info easily. In the same footer in a different section is just a small text snippet that says custom balloons are available to let people know that their options are not limited at all.</p>
+		<h6>Wireframe for mobile</h6>
+		<img src="images/pwp-wf-a.png">
+		<h6>Wireframe for desktop</h6>
+		<img src="images/pwp-wf-b.png">
+	</body>
+</html>
