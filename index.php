5b128641e7d08e689b6bae1455b1864b2ebd7c11
Ben Smith <rios7467@gmail.com>
2017-02-23 18:17:16 -0700

    Added the index file and gave some small starter text.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+
+<html lang="eng">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+		<title>Balloon Cocoons</title>
+	</head>
+	<body>
+		<h1 id="welcome">Welcome to Balloon Cocoons!</h1>
+		<p id="intro">Here you can see a few premade templates of balloons along with pricing to get a grasp of what is possible. Keep in mind custom balloons are easily available all you need to do is contact me down below.</p>
+
+	</body>
+</html>
