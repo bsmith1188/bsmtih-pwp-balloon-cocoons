a9169c56be4163617c7fed6c42a41c9bec34f9cd
Ben Smith <rios7467@gmail.com>
2017-02-23 19:36:45 -0700

    Added a gradient and am writing a js function to randomize it

--- index.php
+++ index.php
@@ -6,16 +6,23 @@
 		<link rel="stylesheet" href="css/style.css" type="text/css"/>
 		<title>Balloon Cocoons</title>
 	</head>
-	<body>
+	<body id="grad">
 		<h1 id="welcome">Welcome to Balloon Cocoons!</h1>
 		<p id="intro">Here you can see a few premade templates of balloons along with pricing to get a grasp of what is possible. Keep in mind custom balloons are easily available all you need to do is contact me down below.</p>
 		<!--setting placeholders for the images to be set for left or right side-->
-		<img class="imgleft">
-		<p class="textleft">Features blah blah blah </p>
-		<img class="imgleft">
-		<img class="imgleft">
-		<img class="imgright">
-		<img class="imgright">
-		<img class="imgright">
+		<img class="imgleft1">
+		<p class="textleft1">Features blah blah blah $15 </p>
+		<img class="imgleft2">
+		<p class="textleft2">Features blah blah blah $15</p>
+		<img class="imgleft3">
+		<p class="textleft3">Features blah blah blah $15</p>
+		<img class="imgright1">
+		<p class="textright1">Features blah blah blah $30</p>
+		<img class="imgright2">
+		<p class="textright2">Features blah blah blah $30</p>
+		<img class="imgright3">
+		<p class="textright3">Features blah blah blah $30</p>
+		<p id="contact-left">Contact me for questions/comments.</p>
+		<p id="contact-right">email: deborah_cade@comcast.net phone: (505)-401-1317</p>
 	</body>
 </html>
---
+++ js/script.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Rios on 2/23/2017.
+ */
