38c86a0d6920d89e8f7a4ad1142518e4f377effc
Ben Smith <rios7467@gmail.com>
2017-02-23 18:31:05 -0700

    Added image tags as placeholders

--- index.php
+++ index.php
@@ -9,6 +9,13 @@
 	<body>
 		<h1 id="welcome">Welcome to Balloon Cocoons!</h1>
 		<p id="intro">Here you can see a few premade templates of balloons along with pricing to get a grasp of what is possible. Keep in mind custom balloons are easily available all you need to do is contact me down below.</p>
-
+		<!--setting placeholders for the images to be set for left or right side-->
+		<img class="imgleft">
+		<p class="textleft">Features blah blah blah </p>
+		<img class="imgleft">
+		<img class="imgleft">
+		<img class="imgright">
+		<img class="imgright">
+		<img class="imgright">
 	</body>
 </html>
