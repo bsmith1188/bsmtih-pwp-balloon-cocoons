668f982f424557a8ba81691964db000a1171bcdd
Ben Smith <rios7467@gmail.com>
2017-02-27 15:18:58 -0700

    Made progress with the footer and adding in pictures and filler data now.

--- index.php
+++ index.php
@@ -3,29 +3,31 @@
 <html lang="eng">
 	<head>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" href="css/style.css" type="text/css"/>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
 		<script src="js/script.js"></script>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
 		<title>Balloon Cocoons</title>
 	</head>
 	<body id="gradient">
-		<h1 id="welcome">Welcome to Balloon Cocoons!</h1>
-		<p class="intro">Here you can see a few premade templates of balloons along with pricing to get a grasp of what is possible. Keep in mind custom balloons are easily available all you need to do is contact me down below.</p>
+		<div class="col-md-12" id="welcome">Welcome to Balloon Cocoons!</div>
+		<div class="col-md-12"></div>
+		<div class="col-md-2"></div><div class="col-md-8" id="intro">Here you can see a few premade templates of balloons along with pricing to get a grasp of what is possible. Keep in mind custom balloons are easily available all you need to do is contact me down below.</div>
 		<!--setting placeholders for the images to be set for left or right side-->
-		<div class="col-md-3"><img class="imgleft1" src=""></div>
-		<div class="textleft1">Features blah blah blah $15 </div>
-		<img class="imgleft2" src="">
-		<p class="textleft2">Features blah blah blah $15</p>
-		<img class="imgleft3" src="">
-		<p class="textleft3">Features blah blah blah $15</p>
-		<img class="imgright1" src="">
-		<p class="textright1">Features blah blah blah $30</p>
-		<img class="imgright2" src="">
-		<p class="textright2">Features blah blah blah $30</p>
-		<img class="imgright3" src="">
-		<div class="">Features blah blah blah $30</div>
-		<div class="col-md-6">Contact me for questions/comments.</div>
-		<div class="col-md-6">email: deborah_cade@comcast.net phone: (505)-401-1317</div>
+		<div class="col-md-3"><img class="imgleft1" src="public_html/images/christmas-floral.jpg"></div>
+		<div class="col-md-3">Features: Christmas style balloon with gingerbread sucker. $20 </div>
+		<div class="col-md-3"><img class="imgleft2" src=""></div>
+		<div class="col-md-3">Features blah blah blah $15</div>
+		<div class="col-md-3"><img class="imgleft3" src=""></div>
+		<div class="col-md-3">Features blah blah blah $15</div>
+		<div class="col-md-3"><img class="imgright1" src=""></div>
+		<div class="col-md-3">Features blah blah blah $30</div>
+		<div class="col-md-3"><img class="imgright2" src=""></div>
+		<div class="col-md-3">Features blah blah blah $30</div>
+		<div class="col-md-3"><img class="imgright3" src=""></div>
+		<div class="col-md-3">Features blah blah blah $30</div>
+		<div class="col-md-4" id="left-info">Contact me for questions/comments.</div>
+		<div class="col-md-4" id="mid-info">(Email:) deborah_cade@comcast.net</div>
+		<div class="col-md-4" id="right-info">(phone:) (505)-401-1317</div>
 	</body>
 </html>
