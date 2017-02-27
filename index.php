11fd4d5abbb891b18e1e4cf522c7730de207bfc8
Ben Smith <rios7467@gmail.com>
2017-02-27 14:11:44 -0700

    Trying to implement bootstrap instead of just using css

--- index.php
+++ index.php
@@ -4,25 +4,28 @@
 	<head>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+		<script src="js/script.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<title>Balloon Cocoons</title>
 	</head>
-	<body id="grad">
+	<body id="gradient">
 		<h1 id="welcome">Welcome to Balloon Cocoons!</h1>
-		<p id="intro">Here you can see a few premade templates of balloons along with pricing to get a grasp of what is possible. Keep in mind custom balloons are easily available all you need to do is contact me down below.</p>
+		<p class="intro">Here you can see a few premade templates of balloons along with pricing to get a grasp of what is possible. Keep in mind custom balloons are easily available all you need to do is contact me down below.</p>
 		<!--setting placeholders for the images to be set for left or right side-->
-		<img class="imgleft1">
-		<p class="textleft1">Features blah blah blah $15 </p>
-		<img class="imgleft2">
+		<div class="col-md-3"><img class="imgleft1" src=""></div>
+		<div class="textleft1">Features blah blah blah $15 </div>
+		<img class="imgleft2" src="">
 		<p class="textleft2">Features blah blah blah $15</p>
-		<img class="imgleft3">
+		<img class="imgleft3" src="">
 		<p class="textleft3">Features blah blah blah $15</p>
-		<img class="imgright1">
+		<img class="imgright1" src="">
 		<p class="textright1">Features blah blah blah $30</p>
-		<img class="imgright2">
+		<img class="imgright2" src="">
 		<p class="textright2">Features blah blah blah $30</p>
-		<img class="imgright3">
-		<p class="textright3">Features blah blah blah $30</p>
-		<p id="contact-left">Contact me for questions/comments.</p>
-		<p id="contact-right">email: deborah_cade@comcast.net phone: (505)-401-1317</p>
+		<img class="imgright3" src="">
+		<div class="">Features blah blah blah $30</div>
+		<div class="col-md-6">Contact me for questions/comments.</div>
+		<div class="col-md-6">email: deborah_cade@comcast.net phone: (505)-401-1317</div>
 	</body>
 </html>
