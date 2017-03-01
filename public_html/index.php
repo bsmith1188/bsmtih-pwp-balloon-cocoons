598c28965c31e867864f6310a01cff4af5657765
Ben Smith <rios7467@gmail.com>
2017-03-01 13:40:21 -0700

    Took head content from Bretts pwp and will cut off fat of what I dont need.

--- public_html/index.php
+++ public_html/index.php
@@ -1,23 +1,48 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
+<html lang="en">
 
-<html lang="eng">
 	<head>
-
-		<meta charset="UTF-8">
+		<!--Special thanks to Brett Gilbert for head content-->
+		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="Personal Website">
-		<meta name="author" content="Brett Gilbert">
-
-		<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>-->
-
-		<script src="js/script.js"></script>
-
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+		<meta name="author" content="Benjamin Smith">
 
 		<title>Balloon Cocoons</title>
+
+		<!-- Latest compiled and minified Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css"
+				rel="stylesheet">
+
+		<!-- MY Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+
+		<!-- Custom CSS -->
+		<link href="css/scrolling-nav.css" rel="stylesheet">
+
+		<!-- Scrolling Nav JavaScript -->
+		<script src="js/jquery.easing.min.js"></script>
+		<script src="js/scrolling-nav.js"></script>
 
 	</head>
 	<body>
@@ -26,19 +51,24 @@
 				Welcome to <span id="balloon-cocoons">Balloon Cocoons</span>
 			</div>
 			<div class="col-md-12" id="intro">
-				This site is a gallery with the decriptions of ballons for sale along with prices. Custom balloons are available and you can even put your own stuff in them! Contact me below for more information on the balloons and pricing details.
+				This site is a gallery with the decriptions of ballons for sale along with prices. Custom balloons are
+				available and you can even put your own stuff in them! Contact me below for more information on the balloons
+				and pricing details.
 			</div>
 
 			<div class="col-md-12" id="most-popular">
 				Most Popular Balloons
 			</div>
-
-
+				<div class="col-md-6">
+					Test
+				</div>
+				<div class="col-md-6">
+					Test row 2
+				</div>
 
 			<div class="col-md-12" id="christmas-balloons">
-			Christmas Style Balloons
+				Christmas Style Balloons
 			</div>
-
 
 
 			<div class="col-md-12" id="valentines-balloons">
@@ -57,6 +87,5 @@
 			<!--footer content-->
 		</footer>
 
-		<div class="col-md-8" id="intro">Here you can see a few premade templates of balloons along with pricing to get a grasp of what is possible. Keep in mind custom balloons are easily available all you need to do is contact me down below.</div>
 	</body>
 </html>
