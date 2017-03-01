9e4fc93cf2645fc8dcd70cfc11aeb1b417f7b9c7
Ben Smith <rios7467@gmail.com>
2017-03-01 13:14:33 -0700

    Starting fresh because this looks like shit.

---
+++ public_html/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+
+<html lang="eng">
+	<head>
+
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Personal Website">
+		<meta name="author" content="Brett Gilbert">
+
+		<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>-->
+
+		<script src="js/script.js"></script>
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+
+		<title>Balloon Cocoons</title>
+
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<!--page content-->
+		</div>
+
+		<footer>
+			<!--footer content-->
+		</footer>
+
+		<div class="col-md-8" id="intro">Here you can see a few premade templates of balloons along with pricing to get a grasp of what is possible. Keep in mind custom balloons are easily available all you need to do is contact me down below.</div>
+	</body>
+</html>
